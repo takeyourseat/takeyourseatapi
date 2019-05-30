25ef20fb46c5a36c47eaa310d08422f3da292f03
cristiduluta <cristi.duluta@outlook.com>
2019-05-30 09:45:40 +0300

    feat(dao) Add PlaceRequest DAO class

---
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/data/entities/PlaceRequest.java
@@ -0,0 +1,141 @@
+package com.stefanini.internship.placemanagement.data.entities;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "place_requests")
+public class PlaceRequest {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "date_of", nullable = false)
+    private java.sql.Timestamp dateOf;
+
+    @Column(name = "place_x")
+    private int placeX;
+
+    @Column(name = "place_y")
+    private int placeY;
+
+    @Column(name = "office_floor")
+    private int officeFloor;
+
+    @Column(name = "office_number")
+    private int officeNumber;
+
+    @Column(name = "office_description")
+    private String officeDescription;
+
+    @Column(name = "approved")
+    private Boolean approved;
+
+    @Column(name = "reviewed_at")
+    private java.sql.Timestamp reviewedAt;
+
+    //region 2 Constructors
+
+    public PlaceRequest() {
+    }
+
+    public PlaceRequest(Long id, @NotNull java.sql.Timestamp dateOf, int requestedX, int requestedY, int requestedOfficeFloor, int getRequestedOfficeNumber, String requestedOfficeDescription, Boolean approved, java.sql.Timestamp reviewedAt) {
+        this.id = id;
+        this.dateOf = dateOf;
+        this.placeX = requestedX;
+        this.placeY = requestedY;
+        this.officeFloor = requestedOfficeFloor;
+        this.officeNumber = getRequestedOfficeNumber;
+        this.officeDescription = requestedOfficeDescription;
+        this.approved = approved;
+        this.reviewedAt = reviewedAt;
+    }
+
+    //endregion
+
+    //region Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public PlaceRequest setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public java.sql.Timestamp getDateOf() {
+        return dateOf;
+    }
+
+    public PlaceRequest setDateOf(java.sql.Timestamp dateOf) {
+        this.dateOf = dateOf;
+        return this;
+    }
+
+    public int getPlaceX() {
+        return placeX;
+    }
+
+    public PlaceRequest setPlaceX(int placeX) {
+        this.placeX = placeX;
+        return this;
+    }
+
+    public int getPlaceY() {
+        return placeY;
+    }
+
+    public PlaceRequest setPlaceY(int placeY) {
+        this.placeY = placeY;
+        return this;
+    }
+
+    public int getOfficeFloor() {
+        return officeFloor;
+    }
+
+    public PlaceRequest setOfficeFloor(int officeFloor) {
+        this.officeFloor = officeFloor;
+        return this;
+    }
+
+    public int getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public PlaceRequest setOfficeNumber(int officeNumber) {
+        this.officeNumber = officeNumber;
+        return this;
+    }
+
+    public String getOfficeDescription() {
+        return officeDescription;
+    }
+
+    public PlaceRequest setOfficeDescription(String officeDescription) {
+        this.officeDescription = officeDescription;
+        return this;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public PlaceRequest setApproved(Boolean approved) {
+        this.approved = approved;
+        return this;
+    }
+
+    public java.sql.Timestamp getReviewedAt() {
+        return reviewedAt;
+    }
+
+    public PlaceRequest setReviewedAt(java.sql.Timestamp reviewedAt) {
+        this.reviewedAt = reviewedAt;
+        return this;
+    }
+
+    //endregion
+}
