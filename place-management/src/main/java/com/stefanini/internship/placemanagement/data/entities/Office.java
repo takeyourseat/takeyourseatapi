875a16380d757936f6668d651ac962133ec240b3
cristiduluta <cristi.duluta@outlook.com>
2019-05-30 09:36:33 +0300

    feat(dao) Office DAO class

---
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/data/entities/Office.java
@@ -0,0 +1,99 @@
+package com.stefanini.internship.placemanagement.data.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "offices")
+public class Office {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true, name = "number")
+    private int number;
+
+    @Column(name = "size_x")
+    private int sizeX;
+
+    @Column(name = "size_y")
+    private int sizeY;
+
+    @Column(name = "floor")
+    private int floor;
+
+    @Column(name = "description")
+    private String description;
+
+    //region 2 Constructors
+
+    public Office() {
+    }
+
+    public Office(Long id, int number, int sizeX, int sizeY, int floor, String description) {
+        this.id = id;
+        this.number = number;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.floor = floor;
+        this.description = description;
+    }
+
+    //endregion
+
+    //region Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public Office setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Office setNumber(int number) {
+        this.number = number;
+        return this;
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public Office setSizeX(int sizeX) {
+        this.sizeX = sizeX;
+        return this;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public Office setSizeY(int sizeY) {
+        this.sizeY = sizeY;
+        return this;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public Office setFloor(int floor) {
+        this.floor = floor;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Office setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    //endregion
+}
