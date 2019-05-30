1ebf8a12975c3fc3ebf6fe0ac6aad9626352462c
cristiduluta <cristi.duluta@outlook.com>
2019-05-30 10:22:49 +0300

    refactor(dao) remove user repository

--- place-management/src/main/java/com/stefanini/internship/placemanagement/data/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stefanini.internship.placemanagement.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.stefanini.internship.placemanagement.data.entities.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
