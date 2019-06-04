d6835bee7834137ec3e1e1439fd4b7c7ae9b6e04
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-04 09:52:40 +0300

    chore(authorization-server) add a temporar WebSecurityConfig that will allow all GET requests from anywhere

---
+++ authorization-server/src/main/java/com/stefanini/internship/authorizationserver/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.stefanini.internship.authorizationserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.GET,"/**").permitAll()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable();
+
+    }
+}
