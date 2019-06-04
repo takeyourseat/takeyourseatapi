b36c7ed944b2700284eb8069ebefc0f3d13652a5
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-04 13:07:00 +0300

    refactor(get-authorization) change parameeter List<Integer> masks to Integer mask

--- authorization-server/src/main/java/com/stefanini/internship/authorizationserver/controllers/RemoteAclServiceController.java
+++ authorization-server/src/main/java/com/stefanini/internship/authorizationserver/controllers/RemoteAclServiceController.java
@@ -2,21 +2,18 @@ package com.stefanini.internship.authorizationserver.controllers;
 
 import com.stefanini.internship.authorizationserver.AuthorizationResponse;
 import com.stefanini.internship.authorizationserver.PublicBasePermission;
-import com.stefanini.internship.authorizationserver.foo.AclJacksonParsableImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.security.acls.domain.ObjectIdentityImpl;
 import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.jdbc.JdbcAclService;
 import org.springframework.security.acls.model.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -30,18 +27,13 @@ public class RemoteAclServiceController {
             @RequestParam String classname,
             @RequestParam Long identifier,
             @RequestParam String principal,
-            @RequestParam List<Integer> mask
+            @RequestParam Integer mask
     ){
         ObjectIdentity objectIdentity = new ObjectIdentityImpl(classname, identifier);
-        List<Sid> sids = new ArrayList<>(1);
-        sids.add(new PrincipalSid(principal));
 
         Acl acl = jdbcAclService.readAclById(objectIdentity, sids);
+        List<Sid> sids = Arrays.asList(new PrincipalSid(principal));
 
-        List<Permission> permission = mask
-                .stream()
-                .map(PublicBasePermission::new)
-                .collect(Collectors.toList());
 
         try{
             boolean authorized = acl.isGranted(permission,sids,false);
