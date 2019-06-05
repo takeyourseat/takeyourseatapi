f4e56fb445030c20f35ac26c17592b3b38a4c831
Eric <gutsueric@gmail.com>
2019-06-05 15:31:50 +0300

    remove(OfficeController) remove some imports

--- place-management/src/main/java/com/stefanini/internship/placemanagement/controller/OfficeController.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/controller/OfficeController.java
@@ -1,15 +1,11 @@
 package com.stefanini.internship.placemanagement.controller;
 
-import com.stefanini.internship.placemanagement.data.entities.Office;
 import com.stefanini.internship.placemanagement.data.repositories.OfficeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
