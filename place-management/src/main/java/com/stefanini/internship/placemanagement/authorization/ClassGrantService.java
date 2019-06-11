cccd52218c7147e17c6fbbe64509f605404a4386
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-11 17:53:00 +0300

    refactor(authorization) rename ClassPermissionService into ClassGrantService

--- place-management/src/main/java/com/stefanini/internship/placemanagement/authorization/ClassGrantService.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/authorization/ClassGrantService.java
@@ -1,9 +1,5 @@
 package com.stefanini.internship.placemanagement.authorization;
 
-import org.springframework.security.acls.domain.BasePermission;
-import org.springframework.security.acls.domain.DefaultPermissionFactory;
-import org.springframework.security.acls.domain.PermissionFactory;
-import org.springframework.security.acls.model.Permission;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -11,17 +7,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import static com.stefanini.internship.placemanagement.authorization.OwaPermissionEvaluator.AUTHORIZATION_SERVER_URI;
 
-@Component("ClassPermissionService")
-public class ClassPermissionService {
+@Component("ClassGrantService")
+public class ClassGrantService {
 
 
     private RestTemplate restTemplate;
 
-    public ClassPermissionService(RestTemplate restTemplate){
+    public ClassGrantService(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
     }
 
-    public boolean hasClassPermission(Authentication auth, String classname, String permission){
+    public boolean hasClassGrant(Authentication auth, String classname, String permission){
         if ((auth == null) || (classname == null) || (permission == null)) {
             return false;
         }
