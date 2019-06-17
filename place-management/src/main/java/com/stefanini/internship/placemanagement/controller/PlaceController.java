4cbb7e5bacdd21cc7548b4c134ac342a6fb65fe9
cristiduluta <cristi.duluta@outlook.com>
2019-06-17 18:41:10 +0300

    feat(place controller) add not found exception in getplacebyid method

--- place-management/src/main/java/com/stefanini/internship/placemanagement/controller/PlaceController.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/controller/PlaceController.java
@@ -2,12 +2,12 @@ package com.stefanini.internship.placemanagement.controller;
 
 import com.stefanini.internship.placemanagement.data.entities.Place;
 import com.stefanini.internship.placemanagement.data.repositories.PlaceRepository;
+import com.stefanini.internship.placemanagement.exception.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api")
@@ -22,12 +22,9 @@ public class PlaceController {
     }
 
     @RequestMapping(value = "/places/{id}", method = RequestMethod.GET)
-    public ResponseEntity getPlaceById(@PathVariable Long id) {
-        if (placeRepository.getPlaceById(id) == null) {
-            return new ResponseEntity<>("place not found", HttpStatus.NOT_FOUND);
-        } else {
-            return ResponseEntity.status(HttpStatus.OK).body(placeRepository.getPlaceById(id));
-        }
+    public ResponseEntity getPlaceById(@PathVariable Long id) throws ResourceNotFound {
+        Place place = placeRepository.findById(id).orElseThrow(() -> new ResourceNotFound("Place with id " + id + " not found"));
+        return ResponseEntity.ok().body(place);
     }
 
     @RequestMapping(value = "/places", method = RequestMethod.POST)
