0202116d0847332a87e76eece04ff2a5d4f4babf
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-18 11:36:38 +0300

    feat(*) add RestTemplate bean to Application class

--- oauth-server/src/main/java/com/stefanini/internship/oauthserver/OauthServerApplication.java
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/OauthServerApplication.java
@@ -2,7 +2,9 @@ package com.stefanini.internship.oauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAuthorizationServer
@@ -12,4 +14,8 @@ public class OauthServerApplication {
 		SpringApplication.run(OauthServerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
