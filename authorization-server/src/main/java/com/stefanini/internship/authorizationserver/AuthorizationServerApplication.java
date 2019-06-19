ba200e764791732e4275c80749ea7ccaef5f919c
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-19 18:16:03 +0300

    feat(rba) add temporairly hardcoded user service

--- authorization-server/src/main/java/com/stefanini/internship/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/stefanini/internship/authorizationserver/AuthorizationServerApplication.java
@@ -2,12 +2,19 @@ package com.stefanini.internship.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
