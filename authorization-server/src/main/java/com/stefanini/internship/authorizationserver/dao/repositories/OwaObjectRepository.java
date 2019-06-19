515d5f62bfcc3637b7d2f68ac906405d3d5071f9
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-19 15:24:00 +0300

    feat(authorization) remove now unused owa repositories

--- authorization-server/src/main/java/com/stefanini/internship/authorizationserver/dao/repositories/OwaObjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stefanini.internship.authorizationserver.dao.repositories;
-
-import com.stefanini.internship.authorizationserver.dao.OwaObject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwaObjectRepository extends JpaRepository<OwaObject, Long> {
-    OwaObject findByOwaClassClassnameAndIdentifier(String classname, Long identifier);
-}
--- authorization-server/src/main/java/com/stefanini/internship/authorizationserver/dao/repositories/OwaRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stefanini.internship.authorizationserver.dao.repositories;
-
-import com.stefanini.internship.authorizationserver.dao.OwaRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwaRoleRepository extends JpaRepository<OwaRole, Long> {
-    OwaRole findByName(String name);
-}
