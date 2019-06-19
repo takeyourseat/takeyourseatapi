de41be5a3a61e7b2aae713aeee99cea877e0399b
Eric <gutsueric@gmail.com>
2019-06-19 14:52:46 +0300

    feature(PlaceRequestRepository) add finding placeRequest by userId, placeId and reviewedAt timestamp

--- place-management/src/main/java/com/stefanini/internship/placemanagement/data/repositories/PlaceRequestRepository.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/data/repositories/PlaceRequestRepository.java
@@ -4,13 +4,14 @@ import com.stefanini.internship.placemanagement.data.entities.PlaceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
 public interface PlaceRequestRepository extends JpaRepository<PlaceRequest, Long> {
     PlaceRequest getPlaceRequestById(Long id);
 
-    PlaceRequest getPlaceRequestByPlaceIdAndUserId(Long placeId, Long userId);
+    PlaceRequest getPlaceRequestByPlaceIdAndUserIdAndReviewedAt(Long placeId, Long userId, Timestamp timestamp);
 
     List<PlaceRequest> getPlaceRequestsByUserId(Long userId);
 
