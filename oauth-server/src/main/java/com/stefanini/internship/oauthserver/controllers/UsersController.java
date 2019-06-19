4966249a78c8392f6d81df84c56061767b927f8b
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-19 12:13:05 +0300

    feat(authentication) add controller for creating and deleting users

---
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/controllers/UsersController.java
@@ -0,0 +1,53 @@
+package com.stefanini.internship.oauthserver.controllers;
+
+import com.stefanini.internship.oauthserver.dao.User;
+import com.stefanini.internship.oauthserver.dao.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
+
+import static com.stefanini.internship.oauthserver.utils.AppConstants.API_ROOT_URL;
+
+@RestController
+@RequestMapping(API_ROOT_URL+"users")
+public class UsersController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PreAuthorize("@ClassGrantService.hasClassGrant('User','write')")
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody User user){
+        user.setAccountNonExpired(true);
+        user.setAccountNonLocked(true);
+        user.setCredentialsNonExpired(true);
+        user.setEnabled(true);
+
+        String password = hashPassword(user.getPassword());
+        user.setPassword(password);
+
+        userRepository.save(user);
+        return ResponseEntity.status(201).build();
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasPermission(#id,'User','write')")
+    public ResponseEntity deactivateUser(@PathVariable Long id){
+        User user = userRepository.getOne(id);
+        if (user == null)
+            return ResponseEntity.notFound().build();
+
+        user.setEnabled(false);
+        userRepository.save(user);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+    static String hashPassword(String password){
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        return "{bcrypt}"+hashed;
+    }
+}
