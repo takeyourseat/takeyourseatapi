60c84ca6c5dd0eb1017e9cf64188f47ed55e15b5
Eric <gutsueric@gmail.com>
2019-06-19 14:58:43 +0300

    refactor(UserRepository) change name method

--- place-management/src/main/java/com/stefanini/internship/placemanagement/data/repositories/UserRepository.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/data/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User getUsersById(Long Id);
+    User getUserById(Long id);
 
     List<User> getUsersByManagerId(Long id);
 }
