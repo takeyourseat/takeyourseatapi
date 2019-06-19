b12d4cce1a6e3bb64e5ce67151c6fe48611aa38f
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-19 15:31:31 +0300

    feat(authorization) add a util method that generates permissions based on their name

--- authorization-server/src/main/java/com/stefanini/internship/authorizationserver/utils/AppConstants.java
+++ authorization-server/src/main/java/com/stefanini/internship/authorizationserver/utils/AppConstants.java
@@ -1,7 +1,20 @@
 package com.stefanini.internship.authorizationserver.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AppConstants {
     public static final String API_ROOT_VERSION = "/api/v01/";
 
-    private AppConstants(){};
+
+    private static final Map<String, Integer> DEFAULTS = new HashMap<>();
+    static{
+        DEFAULTS.put("read", 1);
+        DEFAULTS.put("write", 2);
+    }
+    public static int getPermission(String name){
+        return DEFAULTS.get(name);
+    }
+
+    private AppConstants(){}
 }
