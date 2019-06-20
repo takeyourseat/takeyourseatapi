e3d3ec501c920d82bc79ea81f4ec177c0471ccc7
BondarencoM <bondarenco.mihail@gmail.com>
2019-06-20 12:08:41 +0300

    feat(authorization) replace AuthorizationConstants with AuthorizationUtils

--- user-management/src/main/java/com/stefanini/internship/usermanagement/authorization/AuthorizationConstants.java
+++
@@ -1,5 +0,0 @@
-package com.stefanini.internship.usermanagement.authorization;
-
-public class AuthorizationConstants {
-    public static final String AUTHORIZATION_API = "http://localhost:8086/api/v01/";
-}
