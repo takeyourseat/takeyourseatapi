27b3db03f95f0e997023c8ee50eb38433fd1ad95
lucky4everApostol <cristyapostol93@gmail.com>
2019-06-20 13:54:00 +0300

    added logback.xml file which export 2 different files with logs and set up log tracing using Spring CLoud Sleuth

--- oauth-server/src/main/java/com/stefanini/internship/oauthserver/OauthServerApplication.java
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/OauthServerApplication.java
@@ -1,15 +1,29 @@
 package com.stefanini.internship.oauthserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
+@RestController
 @SpringBootApplication
 @EnableAuthorizationServer
 public class OauthServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthServerApplication.class, args);
+
+		log.error("Message logged at ERROR level {}");
+		log.warn("Message logged at WARN level {}");
+		log.info("Message logged at INFO level {}");
+		log.debug("Message logged at DEBUG level {}");
 	}
+
+
 
 }
---
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/controller/SpringOneController.java
@@ -0,0 +1,31 @@
+package com.stefanini.internship.oauthserver.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@RestController
+public class SpringOneController {
+
+	private final RestTemplate restTemplate;
+
+	public SpringOneController(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@RequestMapping(value = "/springone")
+	public String springOne () {
+		log.info("Before Calling The Server");
+		String response = restTemplate.getForObject("http://localhost:8082/server", String.class);
+		log.info("After Calling The Server");
+
+//		String name ="kk";
+//		if(name.length() == 2) {
+//			throw  new RuntimeException("Opps exception has occured");
+//		}
+
+		return "SPRINGoNE [" + response + "]";
+	}
+}
