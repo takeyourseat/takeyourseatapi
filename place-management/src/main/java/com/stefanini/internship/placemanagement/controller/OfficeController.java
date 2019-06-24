2f858d41e37dba2b1c979a9325cb6dfc08dc99cc
Eric <gutsueric@gmail.com>
2019-06-24 13:28:45 +0300

    refactor(OfficeController) add error handler

--- place-management/src/main/java/com/stefanini/internship/placemanagement/controller/OfficeController.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/controller/OfficeController.java
@@ -1,10 +1,14 @@
 package com.stefanini.internship.placemanagement.controller;
 
+import com.stefanini.internship.placemanagement.data.entities.Office;
 import com.stefanini.internship.placemanagement.data.repositories.OfficeRepository;
+import com.stefanini.internship.placemanagement.exception.ResourceNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Scanner;
 
 @CrossOrigin("http://localhost:4200")
 
@@ -24,11 +28,9 @@ public class OfficeController {
     }
 
     @GetMapping("/offices/{id}")
-    public ResponseEntity getOfficeById(@PathVariable Long id) {
-        if (officeRepository.getOfficeById(id) == null) {
-            return ResponseEntity.notFound().build();
-        } else
-            return ResponseEntity.status(HttpStatus.OK).body(officeRepository.getOfficeById(id));
+    public ResponseEntity getOfficeById(@PathVariable Long id) throws ResourceNotFound {
+        Office office = officeRepository.findById(id).orElseThrow(() -> new ResourceNotFound("Office with id " + id + " not found"));
+        return ResponseEntity.status(HttpStatus.OK).body(office);
     }
 
     @RequestMapping(value = "/offices", params = "floor", method = RequestMethod.GET)
