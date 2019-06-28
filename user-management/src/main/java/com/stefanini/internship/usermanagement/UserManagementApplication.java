02fdcf42ddc93743bda60da686f454730ae134d7
IgorVacarencov <49471246+IgorVacarencov@users.noreply.github.com>
2019-06-28 11:15:42 +0300

    commit for Mihail

--- user-management/src/main/java/com/stefanini/internship/usermanagement/UserManagementApplication.java
+++ user-management/src/main/java/com/stefanini/internship/usermanagement/UserManagementApplication.java
@@ -2,11 +2,10 @@ package com.stefanini.internship.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-=======
->>>>>>> bd1baf1c6002b51f68e8ad08cf6e2c2e5539f410
+
+
 
 @SpringBootApplication
 public class UserManagementApplication {
@@ -15,12 +14,8 @@ public class UserManagementApplication {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
-<<<<<<< HEAD
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-
-=======
->>>>>>> bd1baf1c6002b51f68e8ad08cf6e2c2e5539f410
 }
