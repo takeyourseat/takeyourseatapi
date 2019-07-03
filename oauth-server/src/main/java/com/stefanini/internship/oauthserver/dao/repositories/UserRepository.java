0afe09acdc3d2917af10017e18e6466c585049a2
BondarencoM <bondarenco.mihail@gmail.com>
2019-07-03 11:17:47 +0300

    chore(logs) add logger statements for OAuth Server

--- oauth-server/src/main/java/com/stefanini/internship/oauthserver/dao/repositories/UserRepository.java
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/dao/repositories/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String name);
+    User getByUsername(String name);
 
     boolean existsById(Long id);
 
--- oauth-server/src/main/java/com/stefanini/internship/oauthserver/service/UserValidationService.java
+++ oauth-server/src/main/java/com/stefanini/internship/oauthserver/service/UserValidationService.java
@@ -3,10 +3,14 @@ package com.stefanini.internship.oauthserver.service;
 import com.stefanini.internship.oauthserver.dao.User;
 import com.stefanini.internship.oauthserver.dao.repositories.UserRepository;
 import com.stefanini.internship.oauthserver.exceptions.DuplicateUserException;
+import com.stefanini.internship.oauthserver.exceptions.UserNotFoundException;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserValidationService {
+
+    private final static Logger logger = Logger.getLogger(UserValidationService.class);
 
 
     private UserRepository userRepository;
@@ -16,15 +20,25 @@ public class UserValidationService {
     }
 
     public void assertUnique(User user){
-        if(userRepository.existsById(user.getId()))
-            throw new DuplicateUserException("User ID = "+user.getId()+" is already taken");
+        if(userRepository.existsById(user.getId())) {
+            RuntimeException exception = new DuplicateUserException("User ID = " + user.getId() + " is already taken");
+            logger.error("User ID = " + user.getId() + " is already taken",exception);
+            throw exception;
+        }
 
-        if(userRepository.existsByUsername(user.getUsername()))
-            throw new DuplicateUserException("Username = "+user.getUsername()+" is already taken");
+        if(userRepository.existsByUsername(user.getUsername())) {
+            DuplicateUserException exception = new DuplicateUserException("Username = " + user.getUsername() + " is already taken");
+            logger.error("Username = " + user.getUsername() + " is already taken",exception);
+            throw exception;
+        }
     }
 
-    public void assertWasFound(User user){
-
+    public void assertWasFound(User user, String expected){
+        if(user == null){
+            UserNotFoundException exception = new UserNotFoundException("User with username = " + expected + " could not be found");
+            logger.error("User with username = "+expected+" could not be found",exception);
+            throw exception;
+        }
     }
 
 }
