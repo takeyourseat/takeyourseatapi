2b852a9a296e146e06dad3e2ef0c149eaed5a863
cristiduluta <cristi.duluta@outlook.com>
2019-07-10 12:30:26 +0300

    chore(placeservice) change params in moveUserPlace()

--- place-management/src/main/java/com/stefanini/internship/placemanagement/services/PlaceService.java
+++ place-management/src/main/java/com/stefanini/internship/placemanagement/services/PlaceService.java
@@ -4,6 +4,7 @@ import com.stefanini.internship.placemanagement.data.entities.Office;
 import com.stefanini.internship.placemanagement.data.entities.Place;
 import com.stefanini.internship.placemanagement.data.repositories.OfficeRepository;
 import com.stefanini.internship.placemanagement.data.repositories.PlaceRepository;
+import com.stefanini.internship.placemanagement.exception.NotAvailableException;
 import com.stefanini.internship.placemanagement.exception.OutOfBoundsException;
 import com.stefanini.internship.placemanagement.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -41,21 +42,21 @@ public class PlaceService {
         return place;
     }
 
-    public Place moveUserPlace(Long id, Place place) {
+    public Place moveUserPlace(Integer office, Integer coordinateX, Integer coordinateY, Place place) {
         Place oldPlace = placeRepository.getPlaceByUsername(place.getUsername());
-        Place newPlace = placeRepository.getPlaceById(id);
+        Place newPlace = placeRepository.getPlaceByOfficeNumberAndCoordinateXAndCoordinateY(office, coordinateX, coordinateY);
         if (oldPlace == null) {
             throw new ResourceNotFoundException("Old place can't be found");
         }
         if (newPlace == null) {
             throw new ResourceNotFoundException("New place can't be found");
         } else if (newPlace.getUsername() != null) {
-            throw new ResourceNotFoundException("New place is busy");
+            throw new NotAvailableException("New place is busy");
         } else {
             oldPlace.setUsername(null);
             newPlace.setUsername(place.getUsername());
             placeRepository.save(oldPlace);
-            placeRepository.saveAndFlush(newPlace);
+            placeRepository.save(newPlace);
             return newPlace;
         }
     }
