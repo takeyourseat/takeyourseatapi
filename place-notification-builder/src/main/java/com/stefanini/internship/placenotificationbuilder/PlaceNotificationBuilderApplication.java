2d936b2432e642d9a8594c847bfd2698c4abf6a2
lucky4everApostol <cristyapostol93@gmail.com>
2019-08-05 11:53:51 +0300

    test(Docker-Compose)check jenkins build - plancenotificationbuilder package repetition 2

--- place-notification-builder/src/main/java/com/stefanini/internship/placenotificationbuilder/PlaceNotificationBuilderApplication.java
+++ place-notification-builder/src/main/java/com/stefanini/internship/placenotificationbuilder/PlaceNotificationBuilderApplication.java
@@ -3,8 +3,6 @@ package com.stefanini.internship.placenotificationbuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,8 +12,5 @@ public class PlaceNotificationBuilderApplication {
         SpringApplication.run(PlaceNotificationBuilderApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+
 }
